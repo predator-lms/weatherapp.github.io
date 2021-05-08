feb7dd625b0afef83a627937f47d82cb560d8b6b
Lalit Mohan Srivastav <64480376+predator-lms@users.noreply.github.com>
2021-05-08 22:32:36 +0530

    My weather app

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) =>{
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%weather%}", orgVal.weather[0].main);
+
+    return temperature;
+}
+ const server= http.createServer((req, res) =>{
+     if(req.url =="/"){
+        requests("http://api.openweathermap.org/data/2.5/weather?q=noida&appid=129c247755e48d9a05bbaea98e54c8eb")
+        .on('data', (chunk) => {
+            const objData = JSON.parse(chunk);
+            const arrData = [objData];
+            // console.log(arrData[0].main.temp);
+            const realTimeData = arrData.map(val => replaceVal(homeFile, val)).join("");
+            res.write(realTimeData);
+        })
+        .on('end', (err) => {
+          if (err) return console.log('connection closed due to errors', err);
+         
+          console.log('end');
+          res.end();
+        });
+     }
+
+ });
+
+ server.listen(8000, "127.0.0.1");
